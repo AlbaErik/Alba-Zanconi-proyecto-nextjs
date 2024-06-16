3c1773d859f2cd9c1537434955c4c5a9fbb85e45
AlbaErik <erik.alba97@gmail.com>
2024-06-15 21:13:57 -0300

    Admin dashboard clientsList

--- nextjs-dashboard/src/app/admin/dashboard/customers/page.tsx
+++ nextjs-dashboard/src/app/admin/dashboard/customers/page.tsx
@@ -1,3 +1,12 @@
-export default function Page() {
-    return <p>Customers Data Page</p>;
+import UsersList from '@/app/admin/ui/dashboard/clients';
+import '@/app/admin/ui/dashboard/styleCards.css';
+
+export default async function Page() {
+
+    return (
+        <div>
+            <h1>Users list</h1>
+            <UsersList />
+        </div>
+    );
 }
