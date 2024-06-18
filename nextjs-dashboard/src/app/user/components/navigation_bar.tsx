c9465e7389dc5003cc084b10aa9eb53fe97e0da1
ThiagoZanconi <thiagzanc@gmail.com>
2024-06-17 21:42:05 -0300

    Product card update

--- nextjs-dashboard/src/app/user/components/navigation_bar.tsx
+++ nextjs-dashboard/src/app/user/components/navigation_bar.tsx
@@ -14,16 +14,16 @@ const NavigationBar: React.FC = () => {
                     <Link href={{pathname: `/user/Discos_Duros`,}} className="block px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" aria-current="page">Discos Duros</Link>
                 </li>
                 <li>
-                    <a href="#" className="block px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Auriculares</a>
+                    <Link href={{pathname: `/user/Memorias_RAM`,}} className="block px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Memorias RAM</Link>
                 </li>
                 <li>
                     <Link href={{pathname: `/user/Teclados`,}} className="block px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Teclados</Link>
                 </li>
                 <li>
-                    <a href="#" className="block px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Mouse</a>
+                    <Link href={{pathname: `/user/Fuentes_de_Poder`,}} className="block px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Fuentes</Link>
                 </li>
                 <li>
-                    <a href="#" className="block px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Mousepads</a>
+                    <Link href={{pathname: `/user/Tarjetas_Gráficas`,}} className="block px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Tarjetas</Link>
                 </li>
                 </ul>
             </div>
