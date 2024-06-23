cf7e4e38cb237569928816d998d781e6c704e3c3
ThiagoZanconi <thiagzanc@gmail.com>
2024-06-23 17:12:46 -0300

    Admin log in and log out

--- nextjs-dashboard/src/app/admin/ui/dashboard/sidebar/sidebar.tsx
+++ nextjs-dashboard/src/app/admin/ui/dashboard/sidebar/sidebar.tsx
@@ -1,5 +1,7 @@
 import Link from 'next/link';
 import AdminLinks from './admin-links';
+import { signOut } from '@/auth';
+import { PowerIcon } from '@heroicons/react/24/outline';
 
 export default function SideBar() {
     return (
@@ -15,6 +17,17 @@ export default function SideBar() {
                 <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
 
             </div>
+            <form
+                action={async () => {
+                    'use server';
+                    await signOut();
+                }}
+                >
+                <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
+                    <PowerIcon className="w-6" />
+                    <div className="hidden md:block">Sign Out</div>
+                </button>
+            </form>
         </div>
     );
 }
--- nextjs-dashboard/src/auth.ts
+++ nextjs-dashboard/src/auth.ts
@@ -2,36 +2,29 @@ import NextAuth from 'next-auth';
 import { authConfig } from './auth.config';
 import Credentials from 'next-auth/providers/credentials';
 import { z } from 'zod';
-import type { User } from '@/app/lib/data';
 import bcrypt from 'bcrypt';
-import { sql } from '@vercel/postgres';
+import { getUser } from '@/app/lib/data';
  
-async function getUser(email: string): Promise<User | undefined> {
-  try {
-      const user = await sql<User>`SELECT * FROM users WHERE email=${email}`;
-      return user.rows[0];
-  } catch (error) {
-      console.error('Failed to fetch user:', error);
-      throw new Error('Failed to fetch user.');
-  }
-}
 export const { auth, signIn, signOut } = NextAuth({
   ...authConfig,
   providers: [Credentials({
       async authorize(credentials) {
           const parsedCredentials = z
-              .object({ email: z.string().email(), password: z.string().min(6) })
+              .object({ email: z.string().email(), password: z.string().min(4) })
               .safeParse(credentials);
 
           if (parsedCredentials.success) {
               const { email, password } = parsedCredentials.data;
               const user = await getUser(email);
 
-              if (!user) return null;
+              if (!user){
+                console.log("Usuario no encontrado");
+                return null;
+              } 
               const passwordsMatch = await bcrypt.compare(password, user.password);
               if (passwordsMatch) return user;
           }
-          console.log('Invalid credentials');
+          console.log('Credenciales invalidas');
 
           return null;
       },
