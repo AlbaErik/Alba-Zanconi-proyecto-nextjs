d6cf3cf737fa090bf759a3aff293d4ba745047d3
ThiagoZanconi <thiagzanc@gmail.com>
2024-06-23 21:04:02 -0300

    Sign out button user

--- nextjs-dashboard/src/app/user/layout.tsx
+++ nextjs-dashboard/src/app/user/layout.tsx
@@ -5,8 +5,7 @@ import LoginButton from "./components/login_button"
 import SearchBar from "./components/search_bar"
 import CartButton from "./components/cart_button";
 import { Suspense } from "react";
-import { PowerIcon } from "@heroicons/react/24/outline";
-import { auth, signOut } from "@/auth";
+import SignOutButton from "./components/sign_out_button";
 
 export default function RootLayout({
   children,
@@ -34,21 +33,8 @@ export default function RootLayout({
             <div className="mx-4">
 
                   <LoginButton/>
-         
-                  <form
                   
-                    action={async () => {
-                      'use server';
-                      console.log("LogoutASD");
-                      await signOut({ redirectTo: '/' });
-                    }}
-                      
-                  >
-                    <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
-                        <PowerIcon className="w-6" />
-                        <div className="hidden md:block">Sign Out</div>
-                    </button>
-                  </form>
+                  <SignOutButton/>
             </div>
 
             <div className="h-10">
