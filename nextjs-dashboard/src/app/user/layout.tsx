3a6a0332a94967aaf96509400e00db051989e943
ThiagoZanconi <thiagzanc@gmail.com>
2024-11-24 17:34:50 -0300

    Update layout.tsx

--- nextjs-dashboard/src/app/user/layout.tsx
+++ nextjs-dashboard/src/app/user/layout.tsx
@@ -16,44 +16,41 @@ export default async function RootLayout({
   const session = await auth();
   
   return (
-    <html lang="en">
-      <body>
-        <div className="mx-[3%] sm:mx-[6%] md:mx-[10%] lg:mx-[15%] xl:mx-[20%]">
-
-          <div className= "mt-10 flex items-center">
-            <div className="mr-4">
-              <Image
-                className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-                src="/logo.png"
-                alt="Logo"
-                width={120}
-                height={20}
-                priority
-              />
-            </div>
-            <SearchBar/>
-            <div className="mx-4">
-                <SessionProvider session={session}>
-                  <LoginButton session={session}/>
-                
-                  <SignOutButton session={session}/>
-                </SessionProvider>
-            </div>
-
-            <div className="h-10">
-              <CartButton/>
-            </div>
+    <div>
+      <div className="mx-[3%] sm:mx-[6%] md:mx-[10%] lg:mx-[15%] xl:mx-[20%]">
+        <div className= "mt-10 flex items-center">
+          <div className="mr-4">
+            <Image
+              className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+              src="/logo.png"
+              alt="Logo"
+              width={120}
+              height={20}
+              priority
+            />
+          </div>
+          <SearchBar/>
+          <div className="mx-4">
+              <SessionProvider session={session}>
+                <LoginButton session={session}/>
+              
+                <SignOutButton session={session}/>
+              </SessionProvider>
           </div>
 
-          <div className="mt-4 ">
-            <NavigationBar/>
+          <div className="h-10">
+            <CartButton/>
           </div>
-
         </div>
-        <Suspense>
-           {children}
-        </Suspense>
-      </body>
-    </html>
+
+        <div className="mt-4 ">
+          <NavigationBar/>
+        </div>
+
+      </div>
+      <Suspense>
+          {children}
+      </Suspense>
+    </div>
   );
 }
