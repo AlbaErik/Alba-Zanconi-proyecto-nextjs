fc6b512453c860d7c1175e6879a0caa09d312604
AlbaErik <erik.alba97@gmail.com>
2025-02-14 01:26:22 -0300

    edit product test

--- nextjs-dashboard/tests/product_tests.spec.ts
+++ nextjs-dashboard/tests/product_tests.spec.ts
@@ -1,4 +1,6 @@
 import { test, expect } from '@playwright/test';
+import { title } from 'process';
+import { string } from 'zod';
 
 
 /*
@@ -15,26 +17,16 @@ import { test, expect } from '@playwright/test';
 */
 
 test('Crear un producto tras iniciar sesión', async ({ page }) => {
-  // Ir a la página de login
-  await page.goto('https://alba-zanconi-proyecto-nextjs.vercel.app/account/adminLogin');
-
-  // Llenar el formulario de login
-  await page.fill('input[name="email"]', process.env.ADMIN_USER || 'admin@admin.com');
-  await page.fill('input[name="password"]', process.env.ADMIN_PASSWORD || 'admin');
-
-  // Hacer clic en el botón de login y esperar que redirija al dashboard
-  await page.click('button:has-text("Log in")');
-  await page.waitForTimeout(3000);
-  await page.waitForURL('https://alba-zanconi-proyecto-nextjs.vercel.app/account/adminLogin');
-  
 
   await page.goto('https://alba-zanconi-proyecto-nextjs.vercel.app/admin/dashboard');
+  await page.screenshot({ path: 'screenshot.png', fullPage: true });
 
   // Ir a la página de creación de productos
   await page.goto('https://alba-zanconi-proyecto-nextjs.vercel.app/admin/dashboard/products/create');
 
 
-  const productName = 'Nuevo Producto con Playwright';
+  const productName = '123 Nuevo Producto con Playwright';
+  await page.screenshot({ path: 'screenshot-asdf.png', fullPage: true });
 
   await page.fill('#name', productName);
   await page.fill('#description', 'Es un nuevo producto creado con Playwright');
@@ -52,13 +44,55 @@ test('Crear un producto tras iniciar sesión', async ({ page }) => {
 
   // Buscar el producto
   const searchInput = page.locator('input[placeholder="Ingrese un producto..."]');
-  await searchInput.fill("Nuevo Producto con Playwright"); // Escribe "test" en el input
-  await searchInput.press("Enter"); // Simula presionar Enter para buscar
+  await searchInput.fill("Nuevo Producto con Playwright");
+  await searchInput.press("Enter");
 
 
   const productLocator = page.locator('div.rounded-xl.bg-blue-50').first();
   await expect(productLocator).toContainText("Nuevo Producto con Playwright");
 
+  await page.screenshot({ path: 'screenshot.png', fullPage: true });
+
+});
+
+
+test('Editar un producto tras iniciar sesión', async ({ page }) => {
+
+  await page.goto('https://alba-zanconi-proyecto-nextjs.vercel.app/admin/dashboard/products');
+
+  //Obtengo la primer Card de la lista
+  const card = await page.locator('.rounded-xl.bg-blue-50').first();
+  const firstCardTitle = await card.locator('h3.ml-2.text-sm.font-medium').textContent() || "";
+  await card.locator('button:has-text("Editar Producto")').click();
+
+  // Completo los datos del formulario
+  const productName = "Producto editado con Playwright";
+  const description = "Es un producto editado con Playwright";
+  const price = "100";
+
+  await page.fill('#name', productName);
+  await page.fill('#description', description);
+  await page.fill('#price', price);
+
+  const filePath = 'D:/IAW/Alba-Zanconi-proyecto-nextjs/nextjs-dashboard/public/logo.png';
+  await page.setInputFiles('#file', filePath);
+  await page.selectOption('#category_id', { label: 'Tarjetas Gráficas' });
+
+  //Guardo cambios y confirmo
+  await page.click('button:has-text("Guardar")');
+  await page.waitForTimeout(1000);
+
+  await page.locator('button:text("Si")').waitFor();
+  await page.locator('button:text("Si")').click();
+  await page.waitForTimeout(5000);
+
+  // Buscar el producto
+  const searchInput = page.locator('input[placeholder="Ingrese un producto..."]');
+  await searchInput.fill(productName);
+  await searchInput.press("Enter");
+
+  const productLocator = page.locator('div.rounded-xl.bg-blue-50').first();
+  await expect(productLocator).toContainText(productName);
   await page.screenshot({ path: 'screenshot.png', fullPage: true });
 
 });
